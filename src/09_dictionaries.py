0469733fca59dd11dbf9b32ef250a293a2319fca
Michael Beck <brit228@gmail.com>
2019-05-29 22:45:55 -0400

    Finish assignment

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -2,8 +2,8 @@
 Dictionaries are Python's implementation of associative arrays.
 There's not much different with Python's version compared to what
 you'll find in other languages (though you can also initialize and
-populate dictionaries using comprehensions just like you can with 
-lists!). 
+populate dictionaries using comprehensions just like you can with
+lists!).
 
 The docs can be found here:
 https://docs.python.org/3/tutorial/datastructures.html#dictionaries
@@ -20,12 +20,12 @@ waypoints = [
         "lat": 43,
         "lon": -121,
         "name": "a place"
-    }, 
+    },
     {
         "lat": 41,
         "lon": -123,
         "name": "another place"
-    }, 
+    },
     {
         "lat": 43,
         "lon": -122,
@@ -34,11 +34,21 @@ waypoints = [
 ]
 
 # Add a new waypoint to the list
-# YOUR CODE HERE
+waypoints.append({
+    "lat": 42,
+    "lon": -120,
+    "name": "a new waypoint"
+})
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
-# YOUR CODE HERE
+for w in waypoints:
+    if w["name"] == "a place":
+        w["lon"] = -130
+        w["name"] = "not a real place"
 
 # Write a loop that prints out all the field values for all the waypoints
-# YOUR CODE HERE
+for w in waypoints:
+    for c in w:
+        print(c, '\t', w[c])
+    print()
--- src/14_cal.py
+++ src/14_cal.py
@@ -4,7 +4,7 @@ render a calendar to your terminal.
 https://docs.python.org/3.6/library/calendar.html
 
 Write a program that accepts user input of the form
-  `14_cal.py month [year]`
+  `calendar.py month [year]`
 and does the following:
  - If the user doesn't specify any input, your program should
    print the calendar for the current month. The 'datetime'
@@ -22,3 +22,29 @@ and does the following:
 import sys
 import calendar
 from datetime import datetime
+
+if __name__ == "__main__":
+    error_str = "Invalid format - format of arguments should be"
+    error_str += "in form `[month [year]]` where `month` and `year` are both"
+    error_str += "optional, but `year` requires `month` argument."
+    args = sys.argv
+    if len(args) > 3:
+        print(error_str)
+        exit()
+    year = datetime.now().year
+    month = datetime.now().month
+    if len(args) > 1:
+        try:
+            month = int(args[1])
+            assert(month < 13 and month > 0)
+        except:
+            print(error_str)
+            exit()
+    if len(args) > 2:
+        try:
+            year = int(args[2])
+        except:
+            print(error_str)
+            exit()
+    cal = calendar.TextCalendar()
+    cal.prmonth(year, month)
